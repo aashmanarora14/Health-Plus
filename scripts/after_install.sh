2054f0f4a5631a315131f94a67195beefd3e636f
aashmanarora14 <137777040+aashmanarora14@users.noreply.github.com>
2024-08-26 14:13:45 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd /home/ubuntu/Health-Plus
+<<<<<<< HEAD
+cd /home/ubuntu/frontend
+npm install --force
 npm run build
+=======
+cd /home/ubuntu/pipeline-deploy
+npm install
+>>>>>>> 74418a5965852ae9ddc693c3aa440290b833350b
+
+
